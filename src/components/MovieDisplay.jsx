07e240257b239cb538da1188333419a5bd71f426
jfrisina <jaki.frisina@gmail.com>
2024-07-11 17:04:05 -0700

    refactor code

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,26 +1,24 @@
-const MovieDisplay = ({movie}) => {
-  const loading = () => { 
-    return <h1>No movie to display</h1>
+
+const MovieDisplay = ({ movie }) => {
+
+  // functions created to handle empty content. 
+  // Check alternative conditional rendering options on app.jsx return components 
+  const loading = () => {
+      return <h5>No Movie to Display</h5>
   }
+
   const loaded = () => {
-    return (
-      <>
-      <h1>{movie.Title}</h1>
-      <h2>{movie.Genre}</h2>
-      <img src={movie.Poster} alt={movie.Title} />
-      <h2>{movie.Year}</h2>
-    </>
-    )
-  })
-  // return (
-  //   <>
-  //     <h1>{movie.Title}</h1>
-  //     <h2>{movie.Genre}</h2>
-  //     <img src={movie.Poster} alt={movie.Title} />
-  //     <h2>{movie.Year}</h2>
-  //   </>
-  // )
-  return movie? loaded : loading
+      return (
+          <>
+              <h1>{movie.Title}</h1>
+              <h2>{movie.Genre}</h2>
+              <img src={movie.Poster} alt={movie.Title} />
+              <h2>{movie.Year}</h2>
+          </>
+      )
+  }
+  return movie? loaded() : loading()
 }
 
-export default MovieDisplay
+
+export default MovieDisplay;
